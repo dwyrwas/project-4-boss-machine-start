b9981d0c002c50c626891a34efc417e32e10bf7b
dwyrwas <djwyrwas@gmail.com>
2022-07-24 10:00:37 -0400

    added middleware

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const Router = express.Router();
 
 
 module.exports = app;
@@ -12,13 +13,14 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
