2d62668cc47ac42a98b5392c4b8180736d519e3b
dwyrwas <djwyrwas@gmail.com>
2022-07-25 08:49:25 -0400

    comment routes

--- server/api.js
+++ server/api.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+//define controllers
 const minionsController = require('./minions').default;
 const ideaController = require('./ideas').default;
 const meetingsController = require('./meetings').default;
 
+//minions routes
 apiRouter.get('/minions', minionsController.getMinions);
 apiRouter.post('/minions:minionId', minionsController.updateMinion);
 apiRouter.post('/minions', minionsController.newMinion);
 apiRouter.get('/minion:minionId', minionsController.getMinionById);
 apiRouter.delete('/minion:minionId', minionsController.deleteMinion);
 
+//ideas routes
 apiRouter.get('/ideas', ideaController );
 apiRouter.post('/ideas', ideaController);
 apiRouter.get('/ideas', ideaController);
 apiRouter.post('/ideas', ideaController);
 apiRouter.delete('/ideas', ideaController);
 
+//meetings routes
 apiRouter.get('/meetings', meetingsController);
 apiRouter.post('/meetings', meetingsController);
 apiRouter.delete('/meetings', meetingsController);
